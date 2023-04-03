56ccbe473719c2b4d132a420b83cd1dcc1256b94
MariaCode <m.p.arnaudova@gmail.com>
2023-04-03 16:13:23 +0300

    Set validation for empty input field.

--- green-project/src/components/NewPlants/NewPlants.js
+++ green-project/src/components/NewPlants/NewPlants.js
@@ -9,18 +9,26 @@ export const NewPlants = ({
         typePlant: ''
     }, onPlantSubmit);
 
+    let isTextAreaEmpty = true;
+    
+    const emptyValidation = (values) =>{
+        if(values){
+            isTextAreaEmpty=false;
+        }
+
+        return isTextAreaEmpty;
+    }
     return (   
 
         <div className={styles["form-group form-add-plants"]}>
             {/* <label htmlFor="plants">Decor plants</label> */}
             {/* <!-- <input className={styles["plant-input" type="text" name="plants" id="plants" placeholder="Plant name"> --> */}
-            <form onSubmit={onSubmit} method="POST" action="">
+            <form  className={styles["form-add-plants"]} onSubmit={onSubmit} method="POST" action="">
                 <textarea name="typePlant" className={styles["textarea"]}
                     onChange={changeHandler}
                     value={values.typePlant}
-                    //  value={plantsValues.typePlant}
                     id="plants" cols="20" rows="1"></textarea>
-                <button type="submit" className={styles["add"]}>Add</button>
+                <button type="submit" className={styles["add"]} onClick={() => emptyValidation(values)} >Add</button>
             </form>
 
         </div>
